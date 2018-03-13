d370ae97d9475eebab4d2616f403af23a36d8b97
kolastar2016 <kolastarsky@gmail.com>
2018-03-14 00:46:30 +0300

    nav_bar

---
+++ js/build.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImJ1aWxkLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIiwiZmlsZSI6ImJ1aWxkLmpzIiwic291cmNlc0NvbnRlbnQiOlsiIl19
